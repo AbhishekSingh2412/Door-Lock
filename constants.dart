e7b164b2c3e0f78e334a1aaea6f6b60d2360befc
AbhishekSingh2412 <56639395+AbhishekSingh2412@users.noreply.github.com>
2020-10-30 08:16:49 +0530

    Create constants.dart
    Includes all constants used for stylizing buttons and text.

---
+++ constants.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const kbottomContainerHeight=80.0;
+const kactivecolour=Color(0xFF1D1E33);
+const kinactiveclr=Color(0xFF111328);
+const kbottomContainerColour='blue';
+const kminheight=120.0;
+const kmaxheight=220.0;
+const klabelTextStyle=TextStyle(fontSize: 19.0,
+    color: Color(0xFF8D8E98));
+
+const kheightTextStyle=TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const klargeButtontextStyle=TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle=TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+
+const kresultTextStyle=TextStyle(
+    color: Color(0xFF24D876),
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold);
+
+const kBMITextStyle=TextStyle(
+    fontSize: 100.0,
+    fontWeight: FontWeight.bold);
+
+const kBodyTextStyle=TextStyle(fontSize: 22.0,);
